c335d4fa79213439ac14358dc88651f9c91eb32b
zohaib-khalid770 <zohaib.ch.2208@gmail.com>
2025-06-02 15:58:40 +0500

    update port

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
     host: true,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "http://localhost:3000",
         changeOrigin: true,
         secure: false,
       },

0c80fe524f5f28cf5b25115f620102e5fb18fe7b
zohaib-khalid770 <zohaib.ch.2208@gmail.com>
2025-06-02 16:19:27 +0500

    addsome

--- src/components/redux/messagesapislice.js
+++ src/components/redux/messagesapislice.js
@@ -7,7 +7,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 const baseQuery = fetchBaseQuery({
   baseUrl:
     import.meta.env.MODE === "production"
-      ? "https://hm-bay.vercel.app/api/"
+      ? "https://h-gbackend.vercel.app/api/"
       : "/api",
   credentials: "include",
 });

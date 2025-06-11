1a97d2d2b30c9f608a44daef3848a833d358e9c8
zohaib-khalid770 <zohaib.ch.2208@gmail.com>
2025-06-11 14:35:51 +0500

    add some dta

--- src/components/Herosectoin.jsx
+++ src/components/Herosectoin.jsx
@@ -3,7 +3,7 @@ import showcase from "../assets/images/showcase.mp4"
 
 const HeroSection = () => {
   return (
-    <div className="relative w-full h-[280px] md:h-[600px] lg:h-[600px] bg-darkbg text-white overflow-hidden">
+    <div className="relative w-full h-[350px] md:h-[600px] lg:h-[600px] bg-darkbg text-white overflow-hidden">
       <video
         className="absolute inset-0 w-fit h-fit lg:w-full lg:h-full  object-cover  object-center "
         autoPlay
